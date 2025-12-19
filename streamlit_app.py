b8c1bb4a52ee3a080c7387674e12d873d3565ce2
obrauten <obrauten@gmail.com>
2025-12-19 10:15:37 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="Energisparekalkulator",
+    layout="wide"
+)
+
 # ===============================
 # Konstanter og hjelpefunksjoner
 # ===============================
